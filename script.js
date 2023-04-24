06166aa47a94381f9a67403988968c2c07aa858c
downcats <mikewcurL@gmail.com>
2023-04-24 18:33:44 -0400

    Add search bar to mobile layout

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const toggleButton = document.getElementsByClassName('hamburger-btn')[0];
-const hamburgerLinks = document.getElementsByClassName('hamburger-links')[0];
+const menuButton = document.getElementsByClassName('hamburger-btn')[0];
+const menuLinks = document.getElementsByClassName('hamburger-links')[0];
 
-toggleButton.addEventListener('click', () => {
-    hamburgerLinks.classList.toggle('active');
+menuButton.addEventListener('click', () => {
+    if (searchLinks.classList.contains('active')){
+        searchLinks.classList.toggle('active');
+    }
+    menuLinks.classList.toggle('active');
+})
+
+const searchButton = document.getElementsByClassName('search-btn')[0];
+const searchLinks = document.getElementsByClassName('searchbar')[0];
+
+searchButton.addEventListener('click', () => {
+    if (menuLinks.classList.contains('active')){
+        menuLinks.classList.toggle('active');
+    }
+    searchLinks.classList.toggle('active');
 })
